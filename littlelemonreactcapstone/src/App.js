92791e7b52adda2321ad31f67bfe966bcc2c19a6
Katerina <katekashkobiz@gmail.com>
2024-08-18 20:17:45 +0300

    Add Navigation Bar

--- littlelemonreactcapstone/src/App.js
+++ littlelemonreactcapstone/src/App.js
@@ -1,15 +1,12 @@
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Nav from './components/Nav';
 
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+    <Nav/>
     </>
   );
 }
--- littlelemonreactcapstone/src/components/Nav.js
+++ littlelemonreactcapstone/src/components/Nav.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../images/Logo.svg'
 
-function Nav() {
+const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
     return (
-      <nav>
-        <ul style={{ listStyleType: 'none', display: 'flex', gap: '20px' }}>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#menu">Menu</a></li>
-          <li><a href="#contact">Contact</a></li>
+      <nav className={'navbar ${menuOpen ? "open" : ""}'}>
+        <a href="/"className="logo">
+        <img src={logo} alt="logo"/>
+        </a>
+
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+        </div>
+
+        <ul className={'nav=links ${menuOpen ? "visible" : ""}'}>
+          <li> <a href='/'>Home</a> </li>
+          <li> <a href='/'>About</a> </li>
+          <li> <a href='/'>Services</a> </li>
+          <li> <a href='/'>Menu</a> </li>
+          <li> <a href='/'>Reservations</a> </li>
+          <li> <a href='/'>Order Online</a> </li>
+          <li> <a href='/'>Login</a> </li>
         </ul>
+
+
       </nav>
     );
   }

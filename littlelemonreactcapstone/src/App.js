7d6ee1bc7c406b8db2bdb262deaa945374a88df2
Katerina <katekashkobiz@gmail.com>
2024-08-21 07:56:17 +0300

    Add alert

--- littlelemonreactcapstone/src/App.js
+++ littlelemonreactcapstone/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import Menu from './components/Menu';
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <Router>
     <Nav/>
     <Main/>
+    <Menu/>
     </Router>
   );
 }
